3ab4c003c829adfa0302d4c7c1b40b089cf79b93
Prajjwal <pc732001@gmail.com>
2023-01-27 23:42:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let circle = document.querySelector(".circle")
+let moveBy = 10;
+
+window.addEventListener("load", ()=>{
+    circle.style.position = "absolute";
+    circle.style.left = 0;
+    circle.style.top = 0;
+       
+
+
+});
+
+window.addEventListener("keyup", (e)=>{
+    switch(e.key){
+        case "a":
+            circle.style.left = parseInt(circle.style.left) - moveBy + "px"
+            break;
+
+            case "d":
+            circle.style.left = parseInt(circle.style.left) + moveBy + "px"
+            break;
+
+            case "w":
+            circle.style.top = parseInt(circle.style.top) - moveBy + "px"
+            break;
+
+            case "s":
+            circle.style.top = parseInt(circle.style.top) + moveBy + "px"
+            break;
+    }
+})
